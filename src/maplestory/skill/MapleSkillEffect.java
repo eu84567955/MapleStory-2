6d9185a2b9c3a8aba61428ae2c860af164ad338c
Tyler <tylerhasman10@hotmail.com>
2016-09-30 10:05:07 -0700

    began work on maple stat effect

---
+++ src/maplestory/skill/MapleSkillEffect.java
@@ -0,0 +1,15 @@
+package maplestory.skill;
+
+import maplestory.inventory.item.ItemStatInfo;
+
+public class MapleSkillEffect {
+	
+	private int watk, matk, wdef, mdef, acc, avoid, speed, jump;
+	private int hp, mp;
+	
+	
+	private MapleSkillEffect(){
+		
+	}
+	
+}
